ab8666fbccf6cb11077e06b4cdc27a76acf3b2dd
Enzo Innocenzi <enzo@innocenzi.dev>
2023-01-03 16:35:26 +0100

    style: apply eslint

--- preset.ts
+++ preset.ts
@@ -143,7 +143,7 @@ async function installBase({ autoImports, i18n, icons }: Options) {
 		packages: [
 			'hybridly/laravel',
 			'spatie/laravel-data',
-			'spatie/laravel-typescript-transformer'
+			'spatie/laravel-typescript-transformer',
 		],
 	})
 
@@ -152,20 +152,20 @@ async function installBase({ autoImports, i18n, icons }: Options) {
 		command: 'php',
 		arguments: ['artisan', 'hybridly:install'],
 	})
-	
+
 	await executeCommand({
 		title: 'publish typescript transformer config',
 		command: 'php',
 		arguments: ['artisan', 'vendor:publish', '--tag=typescript-transformer-config'],
 	})
-	
+
 	await editFiles({
 		title: 'update typescript transformer config',
 		files: 'config/typescript-transformer.php',
 		operations: [
 			{
 				type: 'remove-line',
-				match: /Spatie\\TypeScriptTransformer\\Collectors\\DefaultCollector::class,/
+				match: /Spatie\\TypeScriptTransformer\\Collectors\\DefaultCollector::class,/,
 			},
 			{
 				type: 'add-line',
@@ -173,13 +173,13 @@ async function installBase({ autoImports, i18n, icons }: Options) {
 				position: 'after',
 				lines: [
 					'Hybridly\\Support\\TypeScriptTransformer\\DataResourceTypeScriptCollector::class,',
-					'Spatie\\LaravelData\\Support\\TypeScriptTransformer\\DataTypeScriptCollector::class,'
-				]
+					'Spatie\\LaravelData\\Support\\TypeScriptTransformer\\DataTypeScriptCollector::class,',
+				],
 			},
 			{
 				type: 'remove-line',
 				match: /Spatie\\LaravelTypeScriptTransformer\\Transformers\\SpatieStateTransformer::class,/,
-				count: 3
+				count: 3,
 			},
 			{
 				type: 'add-line',
@@ -187,11 +187,11 @@ async function installBase({ autoImports, i18n, icons }: Options) {
 				position: 'after',
 				lines: [
 					'Spatie\\LaravelData\\Support\\TypeScriptTransformer\\DataTypeScriptTransformer::class,',
-					'Spatie\\TypeScriptTransformer\\Transformers\\EnumTransformer::class,'
-				]
-			}
+					'Spatie\\TypeScriptTransformer\\Transformers\\EnumTransformer::class,',
+				],
+			},
 		],
-	});
+	})
 
 	await editFiles({
 		title: 'update welcome route',
