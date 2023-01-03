54baafb0f04178d8b29c7dc24f36a2db2edaaf76
Enzo Innocenzi <enzo@innocenzi.dev>
2023-01-03 16:37:28 +0100

    chore: remove cleanup condition

--- templates/base/resources/application/main.ts
+++ templates/base/resources/application/main.ts
@@ -4,7 +4,6 @@ import 'virtual:hybridly/router'
 import './tailwind.css'
 
 initializeHybridly({
-	cleanup: !import.meta.env.DEV,
 	pages: import.meta.glob('@/views/pages/**/*.vue', { eager: true }),
 	enhanceVue: (vue) => {
 		vue.use(createHead())
