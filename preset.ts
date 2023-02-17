8bfa6709f7b65ffc68bb60f551947c3a0bd8f5cc
Johan van der Molen <hallo@johanvandermolen.nl>
2023-02-17 13:45:00 +0100

    Update preset.ts

--- preset.ts
+++ preset.ts
@@ -418,7 +418,7 @@ async function applyStrictMode() {
 				lines: [
 					'use Carbon\\CarbonImmutable;',
 					'use Illuminate\\Database\\Eloquent\\Model;',
-					'use Illuminate\Database\Eloquent\Relations\Relation;',
+					'use Illuminate\\Database\\Eloquent\\Relations\\Relation;',
 					'use Illuminate\\Foundation\\Console\\CliDumper;',
 					'use Illuminate\\Foundation\\Http\\HtmlDumper;',
 					'use Illuminate\\Support\\Facades\\Date;',
