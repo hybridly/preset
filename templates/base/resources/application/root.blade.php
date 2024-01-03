889b71f7247bfcb100b6093e2aca7999d0745528
Nicolas Hedger <nicolas@hedger.ch>
2024-01-03 12:20:13 +0100

    simplify `@vite` directive

--- templates/base/resources/application/root.blade.php
+++ templates/base/resources/application/root.blade.php
@@ -3,7 +3,7 @@
 	<head>
 			<meta charset="UTF-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			@vite('resources/application/main.ts')
+			@vite
 	</head>
 	<body class="h-full bg-gray-50 antialiased">
 			@hybridly(class: 'h-full')
